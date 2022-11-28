42d14a7e5598459e2197b635ea0b52fd5986581a
Igor <isidoroigor9@gmail.com>
2022-11-28 17:08:16 -0300

    Setup

---
+++ Frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import { BrowserRouter as Router, Route, } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      {/*<Route path="" component={} />*/}
+    </Router>
+  );
+}
+
+export default App;
