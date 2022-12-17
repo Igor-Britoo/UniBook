f655089090f4f4ed60d5654a86806e77b3845090
Igor <isidoroigor9@gmail.com>
2022-12-17 12:43:43 -0300

    fixing .gitignore

--- Backend/UniBook/UniBook/settings.py
+++ Backend/UniBook/UniBook/settings.py
@@ -67,8 +67,8 @@ SIMPLE_JWT = {
 
     'AUTH_HEADER_TYPES': ('Bearer',),
     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-    'USER_ID_FIELD': 'id',
-    'USER_ID_CLAIM': 'user_id',
+    'USER_ID_FIELD': 'email',
+    'USER_ID_CLAIM': 'user_email',
     'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
 
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
@@ -80,8 +80,6 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
-
-    'USER_ID_FIELD': 'email',
 }
 
 MIDDLEWARE = [
