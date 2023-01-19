d425b8094b08be095e674358e5e48fe698d4b614
Igor <isidoroigor9@gmail.com>
2023-01-18 21:09:20 -0300

    Views para visualizar o carrinho do usuário logado e realizar operações de adição, remoção e atualização de seus items

--- Backend/UniBook/base/urls.py
+++ Backend/UniBook/base/urls.py
@@ -12,10 +12,16 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('customer-logged/', views.get_customer_logged, name='get_customer_logged'),
-    path('customer-logged/update/', views.update_customer_logged, name='update_customer_logged'),      
+    path('customer-logged/update/', views.update_customer_logged, name='update_customer_logged'),
+          
     path('customer-logged/orders/', views.get_customer_orders, name='get_customer_orders'),
     path('customer-logged/orders/<uuid:code>/', views.get_customer_order_info, name='get_customer_order_info'),
     path('customer-logged/orders/create/', views.create_customer_order, name='create_customer_order'),
+
+    path('customer-logged/cart/', views.get_customer_cart, name='get_customer_cart'),
+    path('customer-logged/cart/cart-items/create/', views.create_customer_cart_item, name='create_customer_cart_item'),
+    path('customer-logged/cart/cart-items/<int:item_id>/update/', views.update_customer_cart_item, name='update_customer_cart_item'),
+    path('customer-logged/cart/cart-items/<int:item_id>/delete/', views.delete_customer_cart_item, name='delete_customer_cart_item'),
 
     path('books/', views.list_books, name='list_books'),
     path('books/<str:ISBN>/', views.get_book, name='get_book'),
--- Backend/UniBook/base/views.py
+++ Backend/UniBook/base/views.py
@@ -168,6 +168,120 @@ def get_customer_order_info(request, code):
     return Response(status=400)
 
 @api_view(['GET'])
+def get_customer_cart(request):
+    """
+        Returns the cart of the logged in customer
+    """
+    if request.method == 'GET':
+        if(request.auth == None):
+            return Response(status=401)
+
+        else:
+            customer = Customer.objects.filter(email = request.user).first()
+            cart = customer.shopping_cart
+            cart_items = cart.cart_items.all()
+
+            cart_serialized = CartSerializer(cart)
+            cart_items_serialized = CartItemSerializer(cart_items, many = True)
+
+            return Response({
+                'cart': {
+                    **cart_serialized.data,
+                    'cart_items': cart_items_serialized.data,
+                    'owner': customer.email,
+                }
+            }, status=200)
+
+    return Response(status=400)
+
+@api_view(['POST'])
+def create_customer_cart_item(request):
+    """
+        If there is already a cart item in the logged-in customer's cart for the book
+        submitted, increment its quantity by one. Otherwise, create a new cart item
+    """
+    if request.method == 'POST':
+        if(request.auth == None):
+            return Response(status=401)
+
+        else:
+            customer = Customer.objects.filter(email = request.user).first()
+            book = Book.objects.filter(ISBN = request.data['book'])
+
+            if book.exists():
+                book = book.first()
+                cart_item = customer.shopping_cart.cart_items.filter(book = book)
+
+                if cart_item.exists():
+                    cart_item = cart_item.first()
+                    cart_item.quantity += 1
+                    cart_item.save()
+
+                    return Response(status=200)
+
+                else:
+                    cart = customer.shopping_cart
+                    CartItem.objects.create(book= book, 
+                                            quantity = 1,
+                                            cart = cart)
+
+                    return Response(status=201)
+
+            else:
+                return Response(status=404)
+
+    return Response(status=400)
+
+@api_view(['PATCH'])
+def update_customer_cart_item(request, item_id):
+    """
+        If there is an item with the specified id, at logged-in customer cart,
+        update the quantity of this item
+    """
+    if request.method == 'PATCH':
+        if(request.auth == None):
+            return Response(status=401)
+
+        else:
+            customer = Customer.objects.filter(email = request.user).first()
+            cart_item = customer.shopping_cart.cart_items.filter(id = item_id)
+
+            if cart_item.exists():
+                cart_item = cart_item.first()
+                cart_item.quantity = request.data['quantity']
+                cart_item.save()
+
+                return Response(status=200)
+
+            else:
+                return Response(status=404)
+
+    return Response(status=400)
+
+@api_view(['DELETE'])
+def delete_customer_cart_item(request, item_id):
+    """
+        If there is an item with the specified id, at logged-in customer cart,
+        delete it
+    """
+    if request.method == 'DELETE':
+        if(request.auth == None):
+            return Response(status=401)
+
+        else:
+            customer = Customer.objects.filter(email = request.user).first()
+            cart_item = customer.shopping_cart.cart_items.filter(id = item_id)
+
+            if cart_item.exists():
+                cart_item.first().delete()
+                return Response(status=200)
+
+            else:
+                return Response(status=404)
+
+    return Response(status=400)
+
+@api_view(['GET'])
 def list_books(request):
     """
         Returns all books
