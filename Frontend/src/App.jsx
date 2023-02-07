b76db25b3b101124e73ac62c71732db890430bcb
Vitor Mateus <vmateusaguiar@gmail.com>
2023-02-07 17:43:23 -0300

    feat: footer quase feito, falta responsividade

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import './styles/App.css';
 import { AuthProvider } from './contexts/AuthContext';
 import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
+import { Home } from './pages/Home';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
 
           <Route path="/signin/" element= {<SignIn/>} />
           <Route path="/signup/" element= {<SignUp/>} />
+          <Route path="/home" element= {<Home/>} />
           
         </Routes>
       </HashRouter>
