d6e4d7bb866d2d6c57a428ea01fb960690cf9133
Igor <isidoroigor9@gmail.com>
2023-03-18 22:20:30 -0300

    Alterações e correção de bugs nas views do livro

--- Backend/UniBook/base/api/views/books_views.py
+++ Backend/UniBook/base/api/views/books_views.py
@@ -1,4 +1,4 @@
-from django.db.models import Count, Max, Min
+from django.db.models import Count, Max, Min, Q
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -12,8 +12,19 @@ def list_books(request):
     """
         Returns all books
     """
-    limit = int(request.GET.get('limit'))
-    offset = int(request.GET.get('offset'))
+
+    limit = request.GET.get('limit')
+    offset = request.GET.get('offset')
+
+    if not limit:
+        limit = 20
+    else:
+        limit = int(limit)
+    
+    if not offset:
+        offset= 0
+    else:
+        offset = int(offset)
 
     books = Book.objects.all()
 
@@ -22,12 +33,38 @@ def list_books(request):
     page = (offset/limit) + 1
     page_exists =  page > 0 and page <= number_of_pages
 
+    # Getting languages count
+    languages_count = books.values('language').annotate(count=Count('language')).values('language', 'count').filter(count__gt=0)
+
+    # Getting genres count
+    genres_count = []
+    for genre in Genre.objects.all():
+        count = books.filter(genres = genre).count()
+        if count > 0:
+            genres_count.append({ 'genre': genre.name, 'count': count })
+
+    # Getting price and publication year ranges
+    price_range = books.aggregate(min=Min('price'), max=Max('price'))
+    publication_year_range = books.aggregate(min=Min('publication_year'), max=Max('publication_year'))
+
     if page_exists:
         books_page = get_books_page(offset, limit, books)
         books_serialized = BookSerializer(books_page, many=True)
 
-        return Response({ 'number_of_pages' : str(number_of_pages),
-                        'books': books_serialized.data }, status=200)
+        return Response({ 'number_of_pages' : number_of_pages,
+                        'books': books_serialized.data,
+                        'filters': {
+                            'genres': genres_count,
+                            'languages': languages_count,
+                            'price': {
+                                'min': price_range['min'],
+                                'max': price_range['max']
+                            },
+                            'publication_year':{
+                                'min': publication_year_range['min'],
+                                'max': publication_year_range['max']
+                            }
+                        }}, status=200)
     else:
         return Response({ "detail": "Page not found" }, status=404)
 
@@ -41,8 +78,18 @@ def list_books_with_filters(request):
     languages = request.GET.getlist('language')
     price_interval = request.GET.get('price-interval')
     publication_year_interval = request.GET.get('publication-year-interval')
-    limit = int(request.GET.get('limit'))
-    offset = int(request.GET.get('offset'))
+    limit = request.GET.get('limit')
+    offset = request.GET.get('offset')
+
+    if not limit:
+        limit = 20
+    else:
+        limit = int(limit)
+    
+    if not offset:
+        offset= 0
+    else:
+        offset = int(offset)
 
     books = Book.objects.all()
 
@@ -63,13 +110,6 @@ def list_books_with_filters(request):
             books = books.filter(publication_year__range = (publication_year_min, publication_year_max))
         else:
             books = books.filter(publication_year__gte = publication_year_min)
-
-    # Filtering by languages
-    there_is_languages = len(languages) > 0
-    all_languages_selected = len(Book.Language.choices) == len(languages)
-    
-    if there_is_languages and not all_languages_selected:
-        books = books.filter(language__in = languages)
     
     there_is_genres = len(genres) > 0
     all_genres_selected = Genre.objects.all().count() == len(genres)
@@ -77,17 +117,24 @@ def list_books_with_filters(request):
     # Filtering by genres
     if there_is_genres and not all_genres_selected:
         for genre in genres:
-            books = books.filter(genres = genre)  
-
-    # Getting genres count
-    genres = []
-    for genre in Genre.objects.all():
-        count = books.filter(genres = genre ).count()
-        if count > 0:
-            genres.append({ 'genre': genre.name, 'count': count })
+            books = books.filter(genres = genre)
 
     # Getting languages count
-    languages = books.values('language').annotate(count=Count('language')).values('language', 'count').filter(count__gt=0)
+    languages_count = books.values('language').annotate(count=Count('language')).values('language', 'count').filter(count__gt=0)
+
+    # Filtering by languages
+    there_is_languages = len(languages) > 0
+    all_languages_selected = len(Book.Language.choices) == len(languages)
+    
+    if there_is_languages and not all_languages_selected:
+        books = books.filter(language__in = languages)  
+
+    # Getting genres count
+    genres_count = []
+    for genre in Genre.objects.all():
+        count = books.filter(genres = genre).count()
+        if count > 0:
+            genres_count.append({ 'genre': genre.name, 'count': count })
 
     # Getting price and publication year ranges
     price_range = books.aggregate(min=Min('price'), max=Max('price'))
@@ -102,11 +149,11 @@ def list_books_with_filters(request):
         books_page = get_books_page(offset, limit, books)
         books_serialized = BookSerializer(books_page, many=True)
 
-        return Response({ 'number_of_pages' : str(number_of_pages),
+        return Response({ 'number_of_pages' : number_of_pages,
                         'books': books_serialized.data,
                         'filters': {
-                            'genres': genres,
-                            'languages': languages,
+                            'genres': genres_count,
+                            'languages': languages_count,
                             'price': {
                                 'min': price_range['min'],
                                 'max': price_range['max']
@@ -124,38 +171,62 @@ def search_books(request):
     """
         Search books by ISBN, title and author
     """
-    
-    query = request.GET.get('q')
-    limit = int(request.GET.get('limit'))
-    offset = int(request.GET.get('offset'))
-    
-    books_filtered_by_ISBN = Book.objects.filter(ISBN__startswith = query)
-    books_filtered_by_title = Book.objects.filter(title__icontains = query)
-    books_filtered_by_author = Book.objects.filter(author__icontains = query)
 
-    books = books_filtered_by_ISBN.union(books_filtered_by_title, books_filtered_by_author)
+    query = request.GET.get('q')
+    limit = request.GET.get('limit')
+    offset = request.GET.get('offset')
+
+    if not limit:
+        limit = 20
+    else:
+        limit = int(limit)
     
+    if not offset:
+        offset= 0
+    else:
+        offset = int(offset)
+
+    queries = Q(ISBN__startswith = query) | Q(title__icontains = query) | Q(author__icontains = query)
+    books = Book.objects.filter(queries)
+
     number_of_pages = get_number_of_pages(books, limit)
 
     page = (offset/limit) + 1
     page_exists =  page > 0 and page <= number_of_pages
 
-    if page_exists:
-        books_page = get_books_page(offset, limit, books)
-        books_serialized = BookSerializer(books_page, many=True)
+    found_nothing = len(books) == 0
 
-        return Response({ 'number_of_pages' : str(number_of_pages),
-                        'books': books_serialized.data }, status=200)
+    if found_nothing:
+        return Response({ 'detail' : 'Could not find anything for "' + query + '"' }, status=404)
+    
     else:
-        return Response({ "detail": "Page not found" }, status=404)
+        if page_exists:
+            books_page = get_books_page(offset, limit, books)
+            books_serialized = BookSerializer(books_page, many=True)
+
+            return Response({ 'number_of_pages' : number_of_pages,
+                            'books': books_serialized.data }, status=200)
+        else:
+            return Response({ "detail": "Page not found" }, status=404)
 
 @api_view(['GET'])
 def most_viewed_books(request):
     """
         Returns the most viewed books
     """
-    limit = int(request.GET.get('limit'))
-    offset = int(request.GET.get('offset'))
+
+    limit = request.GET.get('limit')
+    offset = request.GET.get('offset')
+
+    if not limit:
+        limit = 20
+    else:
+        limit = int(limit)
+    
+    if not offset:
+        offset= 0
+    else:
+        offset = int(offset)
 
     books = Book.objects.all().order_by('-views')
     
@@ -168,19 +239,28 @@ def most_viewed_books(request):
         books_page = get_books_page(offset, limit, books)
         books_serialized = BookSerializer(books_page, many=True)
 
-        return Response({ 'number_of_pages' : str(number_of_pages),
+        return Response({ 'number_of_pages' : number_of_pages,
                         'books': books_serialized.data }, status=200)
     else:
         return Response({ "detail": "Page not found" }, status=404)
-
 
 @api_view(['GET'])
 def best_sellers(request):
     """
         Returns a list with the best sellers
     """
-    limit = int(request.GET.get('limit'))
-    offset = int(request.GET.get('offset'))
+    limit = request.GET.get('limit')
+    offset = request.GET.get('offset')
+
+    if not limit:
+        limit = 20
+    else:
+        limit = int(limit)
+    
+    if not offset:
+        offset= 0
+    else:
+        offset = int(offset)
 
     books = Book.objects.all().order_by('-sellings')
     
@@ -193,7 +273,7 @@ def best_sellers(request):
         books_page = get_books_page(offset, limit, books)
         books_serialized = BookSerializer(books_page, many=True)
 
-        return Response({ 'number_of_pages' : str(number_of_pages),
+        return Response({ 'number_of_pages' : number_of_pages,
                         'books': books_serialized.data }, status=200)
     else:
         return Response({ "detail": "Page not found" }, status=404)
@@ -203,8 +283,18 @@ def books_on_sale(request):
     """
         Returns the most viewed books
     """
-    limit = int(request.GET.get('limit'))
-    offset = int(request.GET.get('offset'))
+    limit = request.GET.get('limit')
+    offset = request.GET.get('offset')
+
+    if not limit:
+        limit = 20
+    else:
+        limit = int(limit)
+    
+    if not offset:
+        offset= 0
+    else:
+        offset = int(offset)
 
     books = Book.objects.all().order_by('-discount')
 
@@ -217,7 +307,7 @@ def books_on_sale(request):
         books_page = get_books_page(offset, limit, books)
         books_serialized = BookSerializer(books_page, many=True)
 
-        return Response({ 'number_of_pages' : str(number_of_pages),
+        return Response({ 'number_of_pages' : number_of_pages,
                         'books': books_serialized.data }, status=200)
     else:
         return Response({ "detail": "Page not found" }, status=404)
