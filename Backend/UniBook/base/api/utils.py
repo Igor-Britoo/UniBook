843d0b3e65a7e5e5b5703454275617d2f52d8dfc
Igor <isidoroigor9@gmail.com>
2023-03-19 15:06:05 -0300

    Refactor das views do livro e adição da função de filtros as views search_books, most_viewed_books, best_sellers e books_on_sale

--- Backend/UniBook/base/api/utils.py
+++ Backend/UniBook/base/api/utils.py
@@ -1,9 +1,12 @@
-def get_number_of_pages(books, limit):
+from django.db.models import Count, Max, Min
+from ..models import *
+
+def get_number_of_pages(items, limit):
     """
-        Returns the number of books pages based on the limit of books per page.
+        Returns the number of pages based on the limit of items per page.
     """
-    number_of_books = books.count()
-    number_of_pages = number_of_books / limit
+    number_of_items = items.count()
+    number_of_pages = number_of_items / limit
 
     if number_of_pages.is_integer():
         number_of_pages = int(number_of_pages)
@@ -12,15 +15,15 @@ def get_number_of_pages(books, limit):
 
     return number_of_pages
 
-def get_books_page(offset, limit, books):
+def get_page(offset, limit, items):
     """
-        Returns books page based on offset and the limit of books per page.
+        Returns items page based on offset and the limit of items per page.
     """
     start_index = offset
     end_index = offset + limit
-    books = books[start_index:end_index]
+    items = items[start_index:end_index]
     
-    return books
+    return items
 
 def get_values_from_interval_slug(interval_slug):
     """
@@ -33,3 +36,76 @@ def get_values_from_interval_slug(interval_slug):
         max = None
 
     return (min, max)
+
+def filter_books(request, books):
+    """
+        Filter books by price interval, publication year interval, genres and languages and, 
+        returns a QuerySet of books and their possible filters.
+    """
+
+    genres = request.GET.getlist('genre')
+    languages = request.GET.getlist('language')
+    price_interval = request.GET.get('price-interval')
+    publication_year_interval = request.GET.get('publication-year-interval')
+
+    # Filtering by price interval
+    if price_interval:
+        price_min, price_max = get_values_from_interval_slug(price_interval)
+
+        if price_max is not None :
+            books = books.filter(price__range = (price_min, price_max))
+        else:
+            books = books.filter(price__gte = price_min)  
+
+    # Filtering by price publication year interval
+    if publication_year_interval:
+        publication_year_min, publication_year_max = get_values_from_interval_slug(publication_year_interval)
+
+        if publication_year_max is not None:
+            books = books.filter(publication_year__range = (publication_year_min, publication_year_max))
+        else:
+            books = books.filter(publication_year__gte = publication_year_min)
+    
+    there_is_genres = len(genres) > 0
+    all_genres_selected = Genre.objects.all().count() == len(genres)
+
+    # Filtering by genres
+    if there_is_genres and not all_genres_selected:
+        for genre in genres:
+            books = books.filter(genres = genre)
+
+    # Getting languages count
+    languages_count = books.values('language').annotate(count=Count('language')).values('language', 'count').filter(count__gt=0)
+
+    # Filtering by languages
+    there_is_languages = len(languages) > 0
+    all_languages_selected = len(Book.Language.choices) == len(languages)
+    
+    if there_is_languages and not all_languages_selected:
+        books = books.filter(language__in = languages)  
+
+    # Getting genres count
+    genres_count = []
+    for genre in Genre.objects.all():
+        count = books.filter(genres = genre).count()
+        if count > 0:
+            genres_count.append({ 'genre': genre.name, 'count': count })
+
+    # Getting price and publication year ranges
+    price_range = books.aggregate(min=Min('price'), max=Max('price'))
+    publication_year_range = books.aggregate(min=Min('publication_year'), max=Max('publication_year'))
+
+    filters= {
+                'genres': genres_count,
+                'languages': languages_count,
+                'price': {
+                    'min': price_range['min'],
+                    'max': price_range['max']
+                },
+                'publication_year':{
+                    'min': publication_year_range['min'],
+                    'max': publication_year_range['max']
+                }
+            }
+
+    return (books, filters)
--- Backend/UniBook/base/api/views/books_views.py
+++ Backend/UniBook/base/api/views/books_views.py
@@ -10,7 +10,7 @@ from ..utils import *
 @api_view(['GET'])
 def list_books(request):
     """
-        Returns all books
+        Returns all books and their possible filters
     """
 
     limit = request.GET.get('limit')
@@ -48,7 +48,7 @@ def list_books(request):
     publication_year_range = books.aggregate(min=Min('publication_year'), max=Max('publication_year'))
 
     if page_exists:
-        books_page = get_books_page(offset, limit, books)
+        books_page = get_page(offset, limit, books)
         books_serialized = BookSerializer(books_page, many=True)
 
         return Response({ 'number_of_pages' : number_of_pages,
@@ -66,18 +66,15 @@ def list_books(request):
                             }
                         }}, status=200)
     else:
-        return Response({ "detail": "Page not found" }, status=404)
+        return Response({ "detail": "Page not found" }, status=404)    
 
 @api_view(['GET'])
 def list_books_with_filters(request):
     """
-        Filter books by price interval, publication year interval, genres and languages
+        Filter books by price interval, publication year interval, genres and languages and, returns
+        a list of books and their possible filters
     """
 
-    genres = request.GET.getlist('genre')
-    languages = request.GET.getlist('language')
-    price_interval = request.GET.get('price-interval')
-    publication_year_interval = request.GET.get('publication-year-interval')
     limit = request.GET.get('limit')
     offset = request.GET.get('offset')
 
@@ -92,53 +89,8 @@ def list_books_with_filters(request):
         offset = int(offset)
 
     books = Book.objects.all()
-
-    # Filtering by price interval
-    if price_interval:
-        price_min, price_max = get_values_from_interval_slug(price_interval)
-
-        if price_max is not None :
-            books = books.filter(price__range = (price_min, price_max))
-        else:
-            books = books.filter(price__gte = price_min)  
-
-    # Filtering by price publication year interval
-    if publication_year_interval:
-        publication_year_min, publication_year_max = get_values_from_interval_slug(publication_year_interval)
-
-        if publication_year_max is not None:
-            books = books.filter(publication_year__range = (publication_year_min, publication_year_max))
-        else:
-            books = books.filter(publication_year__gte = publication_year_min)
     
-    there_is_genres = len(genres) > 0
-    all_genres_selected = Genre.objects.all().count() == len(genres)
-
-    # Filtering by genres
-    if there_is_genres and not all_genres_selected:
-        for genre in genres:
-            books = books.filter(genres = genre)
-
-    # Getting languages count
-    languages_count = books.values('language').annotate(count=Count('language')).values('language', 'count').filter(count__gt=0)
-
-    # Filtering by languages
-    there_is_languages = len(languages) > 0
-    all_languages_selected = len(Book.Language.choices) == len(languages)
-    
-    if there_is_languages and not all_languages_selected:
-        books = books.filter(language__in = languages)  
-
-    # Getting genres count
-    genres_count = []
-    for genre in Genre.objects.all():
-        count = books.filter(genres = genre).count()
-        if count > 0:
-            genres_count.append({ 'genre': genre.name, 'count': count })
-
-    # Getting price and publication year ranges
-    price_range = books.aggregate(min=Min('price'), max=Max('price'))
-    publication_year_range = books.aggregate(min=Min('publication_year'), max=Max('publication_year'))
+    books, filters = filter_books(request, books)
 
     # Getting the required page
     number_of_pages = get_number_of_pages(books, limit)
@@ -146,30 +98,22 @@ def list_books_with_filters(request):
     page_exists =  page > 0 and page <= number_of_pages
 
     if page_exists:
-        books_page = get_books_page(offset, limit, books)
+        books_page = get_page(offset, limit, books)
         books_serialized = BookSerializer(books_page, many=True)
 
         return Response({ 'number_of_pages' : number_of_pages,
                         'books': books_serialized.data,
-                        'filters': {
-                            'genres': genres_count,
-                            'languages': languages_count,
-                            'price': {
-                                'min': price_range['min'],
-                                'max': price_range['max']
-                            },
-                            'publication_year':{
-                                'min': publication_year_range['min'],
-                                'max': publication_year_range['max']
-                            }
-                        }}, status=200)
+                        'filters': filters
+                        }, status=200)
     else:
         return Response({ "detail": "Page not found" }, status=404)
-        
+
 @api_view(['GET'])
 def search_books(request):
     """
-        Search books by ISBN, title and author
+        Search books by ISBN, title and author and, returns the found books and their possible filters.
+        If the query parameters has genres, languages, price interval or publication year interval, filters
+        the found books.
     """
 
     query = request.GET.get('q')
@@ -186,33 +130,42 @@ def search_books(request):
     else:
         offset = int(offset)
 
-    queries = Q(ISBN__startswith = query) | Q(title__icontains = query) | Q(author__icontains = query)
-    books = Book.objects.filter(queries)
+    if not query:
+        return Response({ 'detail' : 'Cannot search without a query' }, status=400)
 
-    number_of_pages = get_number_of_pages(books, limit)
-
-    page = (offset/limit) + 1
-    page_exists =  page > 0 and page <= number_of_pages
-
-    found_nothing = len(books) == 0
-
-    if found_nothing:
-        return Response({ 'detail' : 'Could not find anything for "' + query + '"' }, status=404)
-    
     else:
-        if page_exists:
-            books_page = get_books_page(offset, limit, books)
-            books_serialized = BookSerializer(books_page, many=True)
+        queries = Q(ISBN__startswith = query) | Q(title__icontains = query) | Q(author__icontains = query)
+        books = Book.objects.filter(queries)
 
-            return Response({ 'number_of_pages' : number_of_pages,
-                            'books': books_serialized.data }, status=200)
+        found_nothing = len(books) == 0
+
+        if found_nothing:
+            return Response({ 'detail' : 'Could not find anything for "' + query + '"' }, status=404)
+        
         else:
-            return Response({ "detail": "Page not found" }, status=404)
+            books, filters = filter_books(request, books)
+
+            number_of_pages = get_number_of_pages(books, limit)
+
+            page = (offset/limit) + 1
+            page_exists =  page > 0 and page <= number_of_pages
+
+            if page_exists:
+                books_page = get_page(offset, limit, books)
+                books_serialized = BookSerializer(books_page, many=True)
+
+                return Response({ 'number_of_pages' : number_of_pages,
+                                'books': books_serialized.data,
+                                'filters': filters
+                                }, status=200)
+            else:
+                return Response({ "detail": "Page not found" }, status=404)
 
 @api_view(['GET'])
 def most_viewed_books(request):
     """
-        Returns the most viewed books
+        Returns the most viewed books and their possible filters. If the query parameters has genres,
+        languages, price interval or publication year interval, filters the sorted books.
     """
 
     limit = request.GET.get('limit')
@@ -230,25 +183,31 @@ def most_viewed_books(request):
 
     books = Book.objects.all().order_by('-views')
     
-    number_of_pages = get_number_of_pages(books, limit)
+    books, filters = filter_books(request, books)
 
+    # Getting the required page
+    number_of_pages = get_number_of_pages(books, limit)
     page = (offset/limit) + 1
     page_exists =  page > 0 and page <= number_of_pages
 
     if page_exists:
-        books_page = get_books_page(offset, limit, books)
+        books_page = get_page(offset, limit, books)
         books_serialized = BookSerializer(books_page, many=True)
 
         return Response({ 'number_of_pages' : number_of_pages,
-                        'books': books_serialized.data }, status=200)
+                        'books': books_serialized.data,
+                        'filters': filters
+                        }, status=200)
     else:
         return Response({ "detail": "Page not found" }, status=404)
 
 @api_view(['GET'])
 def best_sellers(request):
     """
-        Returns a list with the best sellers
+        Returns a list with the best sellers and their possible filters. If the query parameters has genres,
+        languages, price interval or publication year interval, filters the sorted books.
     """
+
     limit = request.GET.get('limit')
     offset = request.GET.get('offset')
 
@@ -264,25 +223,31 @@ def best_sellers(request):
 
     books = Book.objects.all().order_by('-sellings')
     
-    number_of_pages = get_number_of_pages(books, limit)
+    books, filters = filter_books(request, books)
 
+    # Getting the required page
+    number_of_pages = get_number_of_pages(books, limit)
     page = (offset/limit) + 1
     page_exists =  page > 0 and page <= number_of_pages
 
     if page_exists:
-        books_page = get_books_page(offset, limit, books)
+        books_page = get_page(offset, limit, books)
         books_serialized = BookSerializer(books_page, many=True)
 
         return Response({ 'number_of_pages' : number_of_pages,
-                        'books': books_serialized.data }, status=200)
+                        'books': books_serialized.data,
+                        'filters': filters
+                        }, status=200)
     else:
         return Response({ "detail": "Page not found" }, status=404)
 
 @api_view(['GET'])
 def books_on_sale(request):
     """
-        Returns the most viewed books
+        Returns the most viewed books and their possible filters. If the query parameters has genres,
+        languages, price interval or publication year interval, filters the sorted books.
     """
+
     limit = request.GET.get('limit')
     offset = request.GET.get('offset')
 
@@ -298,17 +263,21 @@ def books_on_sale(request):
 
     books = Book.objects.all().order_by('-discount')
 
-    number_of_pages = get_number_of_pages(books, limit)
+    books, filters = filter_books(request, books)
 
+    # Getting the required page
+    number_of_pages = get_number_of_pages(books, limit)
     page = (offset/limit) + 1
     page_exists =  page > 0 and page <= number_of_pages
 
     if page_exists:
-        books_page = get_books_page(offset, limit, books)
+        books_page = get_page(offset, limit, books)
         books_serialized = BookSerializer(books_page, many=True)
 
         return Response({ 'number_of_pages' : number_of_pages,
-                        'books': books_serialized.data }, status=200)
+                        'books': books_serialized.data,
+                        'filters': filters
+                        }, status=200)
     else:
         return Response({ "detail": "Page not found" }, status=404)
 
