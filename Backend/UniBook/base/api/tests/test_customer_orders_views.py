0609e444d9f8bcebfbbbf4e8d6002c471b007f0d
Igor <isidoroigor9@gmail.com>
2023-04-07 17:24:49 -0300

    Update da documentação do endpoint de create_order e dos testes unitários dos endpoints de orders

--- Backend/UniBook/base/api/tests/test_customer_orders_views.py
+++ Backend/UniBook/base/api/tests/test_customer_orders_views.py
@@ -3,19 +3,18 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 
 from ...models import *
-
 class CreateCustomerOrder(APITestCase):
     def setUp(self):
         self.url= "/api/customer-logged/orders/create/"
 
-        customer_address = Address.objects.create( street_name = "test street",
+        self.shipping_address = Address.objects.create( street_name = "test street",
                                                     house_number = 0,
                                                     city = "test city",
                                                     state = Address.State.PE)      
         self.customer = Customer.objects.create_customer(email = "customer@example.com",
                                                         name = "customer name",
                                                         password = "password", 
-                                                        address = customer_address)
+                                                        address = None)
         self.cart = Cart.objects.create(owner = self.customer)
 
         self.genre_1 = Genre.objects.create(
@@ -39,28 +38,43 @@ class CreateCustomerOrder(APITestCase):
             "password": "password"
         })
         self.token = (json.loads(tokens.content))['access']
-        
-    def test_create_customer_order_with_items_on_cart_with_valid_token(self):
+
+    def test_create_customer_order_from_cart_items_with_items_on_cart_with_shipping_address_with_valid_token(self):
         CartItem.objects.create(book= self.book_1, 
                                 quantity = 1,
                                 cart = self.cart)
         
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
-        response = self.client.post(self.url)
+        response = self.client.post(self.url, {
+            "create_from_cart_items" : True,
+            "address":{
+                "street_name": self.shipping_address.street_name,
+                "house_number": self.shipping_address.house_number,
+                "city": self.shipping_address.city,
+                "state": self.shipping_address.state
+            }
+        })
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
 
         response_data = json.loads(response.content)
         expected_data = {'detail': 'Order successfully created'}
         self.assertEqual(response_data, expected_data)
-    
-    def test_create_customer_order_with_items_on_cart_with_invalid_token(self):
+
+    def test_create_customer_order_from_cart_items_with_items_on_cart_with_shipping_address_with_invalid_token(self):
         CartItem.objects.create(book= self.book_1, 
                                 quantity = 1,
                                 cart = self.cart)
         
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token + "x")
-        response = self.client.post(self.url)
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token + 'x')
+        response = self.client.post(self.url, {
+            "create_from_cart_items" : True,
+            "address":{
+                "street_name": self.shipping_address.street_name,
+                "house_number": self.shipping_address.house_number,
+                "city": self.shipping_address.city,
+                "state": self.shipping_address.state
+            }
+        })
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
         response_data = json.loads(response.content)
@@ -77,25 +91,98 @@ class CreateCustomerOrder(APITestCase):
         }
         self.assertEqual(response_data, expected_data)
 
-    def test_create_customer_order_with_items_on_cart_without_token(self):
+    def test_create_customer_order_from_cart_items_with_items_on_cart_with_shipping_address_without_token(self):
         CartItem.objects.create(book= self.book_1, 
                                 quantity = 1,
                                 cart = self.cart)
         
-        response = self.client.post(self.url)
+        response = self.client.post(self.url, {
+            "create_from_cart_items" : True,
+            "address":{
+                "street_name": self.shipping_address.street_name,
+                "house_number": self.shipping_address.house_number,
+                "city": self.shipping_address.city,
+                "state": self.shipping_address.state
+            }
+        })
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
         response_data = json.loads(response.content)
         expected_data = { "detail": "Unauthorized access" }
         self.assertEqual(response_data, expected_data)
 
-    def test_create_customer_order_without_items_on_cart_with_valid_token(self):
+    def test_create_customer_order_from_cart_items_without_items_on_cart_with_shipping_address_with_valid_token(self):
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
-        response = self.client.post(self.url)
+        response = self.client.post(self.url, {
+            "create_from_cart_items" : True,
+            "address":{
+                "street_name": self.shipping_address.street_name,
+                "house_number": self.shipping_address.house_number,
+                "city": self.shipping_address.city,
+                "state": self.shipping_address.state
+            }
+        })
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         response_data = json.loads(response.content)
-        expected_data = {'detail': 'Cannot create an order without cart items'}
+        expected_data = {'detail': 'Cannot create an order without items'}
+        self.assertEqual(response_data, expected_data)
+
+    def test_create_customer_order_from_cart_items_with_items_on_cart_without_shipping_address_with_valid_token(self):
+        CartItem.objects.create(book= self.book_1, 
+                                quantity = 1,
+                                cart = self.cart)
+        
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+        response = self.client.post(self.url, {
+            "create_from_cart_items" : True,
+            "address":{}
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response_data = json.loads(response.content)
+        expected_data = {'detail': 'Cannot create an order without a shipping address'}
+        self.assertEqual(response_data, expected_data)
+
+    def test_create_customer_order_with_items_with_shipping_address_with_valid_token(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+        response = self.client.post(self.url, {
+            "create_from_cart_items" : False,
+            "address":{
+                "street_name": self.shipping_address.street_name,
+                "house_number": self.shipping_address.house_number,
+                "city": self.shipping_address.city,
+                "state": self.shipping_address.state
+            },
+            "items": [
+                {
+                    "ISBN": self.book_1.ISBN,
+                    "quantity": 2,
+                }
+            ]
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        response_data = json.loads(response.content)
+        expected_data = {'detail': 'Order successfully created'}
+        self.assertEqual(response_data, expected_data)
+
+    def test_create_customer_order_without_items_with_shipping_address_with_valid_token(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+        response = self.client.post(self.url, {
+            "create_from_cart_items" : False,
+            "address":{
+                "street_name": self.shipping_address.street_name,
+                "house_number": self.shipping_address.house_number,
+                "city": self.shipping_address.city,
+                "state": self.shipping_address.state
+            },
+            "items": []
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response_data = json.loads(response.content)
+        expected_data = {'detail': 'Cannot create an order without items'}
         self.assertEqual(response_data, expected_data)
 
 class GetCustomerOrders(APITestCase):
@@ -139,9 +226,9 @@ class GetCustomerOrders(APITestCase):
         self.token = (json.loads(tokens.content))['access']
 
     def test_get_customer_orders_where_customer_has_orders_with_valid_page_with_valid_token(self):
-        order = Order.objects.create_order(customer= self.customer)
+        order = Order.objects.create_order(customer= self.customer, shipping_address= self.customer.address)
         OrderItem.objects.create_order_items(order= order,
-                                            cart_items= self.customer.shopping_cart.cart_items.all())
+                                            items= self.customer.shopping_cart.cart_items.all())
         Order.objects.update_order_price(code = order.code)
 
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
@@ -153,9 +240,9 @@ class GetCustomerOrders(APITestCase):
         self.assertNotEqual(response_data, not_expected_data)
 
     def test_get_customer_orders_where_customer_has_orders_with_invalid_page_with_valid_token(self):
-        order = Order.objects.create_order(customer= self.customer)
+        order = Order.objects.create_order(customer= self.customer, shipping_address= self.customer.address)
         OrderItem.objects.create_order_items(order= order,
-                                            cart_items= self.customer.shopping_cart.cart_items.all())
+                                            items= self.customer.shopping_cart.cart_items.all())
         Order.objects.update_order_price(code = order.code)
 
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
@@ -167,9 +254,9 @@ class GetCustomerOrders(APITestCase):
         self.assertEqual(response_data, not_expected_data)
 
     def test_get_customer_orders_where_customer_has_orders_with_valid_page_with_invalid_token(self):
-        order = Order.objects.create_order(customer= self.customer)
+        order = Order.objects.create_order(customer= self.customer, shipping_address= self.customer.address)
         OrderItem.objects.create_order_items(order= order,
-                                            cart_items= self.customer.shopping_cart.cart_items.all())
+                                            items= self.customer.shopping_cart.cart_items.all())
         Order.objects.update_order_price(code = order.code)
 
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token + 'x')
@@ -191,9 +278,9 @@ class GetCustomerOrders(APITestCase):
         self.assertEqual(response_data, expected_data)
 
     def test_get_customer_orders_where_customer_has_orders_with_valid_page_without_token(self):
-        order = Order.objects.create_order(customer= self.customer)
+        order = Order.objects.create_order(customer= self.customer, shipping_address= self.customer.address)
         OrderItem.objects.create_order_items(order= order,
-                                            cart_items= self.customer.shopping_cart.cart_items.all())
+                                            items= self.customer.shopping_cart.cart_items.all())
         Order.objects.update_order_price(code = order.code)
 
         response = self.client.get(self.url + '?limit=1&offset=0')
@@ -246,9 +333,9 @@ class GetCustomerOrderInfo(APITestCase):
                                 quantity = 1,
                                 cart = self.cart )
         
-        self.order = Order.objects.create_order(customer= self.customer)
+        self.order = Order.objects.create_order(customer= self.customer, shipping_address= self.customer.address)
         OrderItem.objects.create_order_items(order= self.order,
-                                            cart_items= self.customer.shopping_cart.cart_items.all())
+                                            items= self.customer.shopping_cart.cart_items.all())
         Order.objects.update_order_price(code = self.order.code)
 
         tokens = self.client.post("/api/token/", {
